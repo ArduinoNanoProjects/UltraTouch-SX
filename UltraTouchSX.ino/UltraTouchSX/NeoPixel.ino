dc18d498e16476d51f456eb02364f193611364d3
McCouman <support@wikibyte.org>
2018-10-05 20:42:38 +0200

    NeoPixel Functions
    Bessere Dokumentation der NeoPixel Funktionen

--- UltraTouchSX.ino/UltraTouchSX/NeoPixel.ino
+++ UltraTouchSX.ino/UltraTouchSX/NeoPixel.ino
@@ -2,6 +2,8 @@
  * Schaltet die LED rgb nach sein Farben an oder aus
  * und ermöglicht die Helligkeit festzulegen
  * 
+ * Boden- und Deckenbeleuchtung
+ * 
  * neo(255, 255, 255, 1);
  */
 void NeoTop(int r, int g, int b){
@@ -18,25 +20,16 @@ void NeoBottom(int r, int g, int b){
   delay(1); 
 }
 
-void AllPixel(int r, int g, int b){
-  pixels.setPixelColor(0, pixels.Color(r,g,b));
-  pixels.setPixelColor(1, pixels.Color(r,g,b));
-  pixels.setPixelColor(4, pixels.Color(r,g,b));
-  pixels.setPixelColor(3, pixels.Color(r,g,b));
-  pixels.setPixelColor(2, pixels.Color(r,g,b));
-  pixels.show();
-  delay(1); 
-}
 
 /**
- * Test Rainbow
+ * Regenbogen der NeoPixel beim Starten des Arduinos
  */
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
-  unsigned long ledMillis = 1;      // Zeit für den Blinkrythmus der LED
-  int cycle = 5;
+  unsigned long ledMillis = 1;      // Blinkzeit
+  int cycle = 5;                    // Wiederholungen
 
-  for(j=0; j<256*cycle; j++) { // 5 cycles of all colors on wheel
+  for(j=0; j<256*cycle; j++) {
     for(i=0; i< pixels.numPixels(); i++) {
       pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
     }
@@ -44,13 +37,10 @@ void rainbowCycle(uint8_t wait) {
     delay(5);
     if (millis() - ledMillis > wait)
     {
-      ledMillis = millis();               // beginne die Zeitzählung von vorne
+      ledMillis = millis();
     }
   }
 }
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
