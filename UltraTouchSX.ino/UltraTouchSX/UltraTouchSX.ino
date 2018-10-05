981f574c22559bf4147c2a48fdac7706e283bf7b
McCouman <support@wikibyte.org>
2018-10-05 19:22:19 +0200

    LicInfos

--- UltraTouchSX.ino/UltraTouchSX/UltraTouchSX.ino
+++ UltraTouchSX.ino/UltraTouchSX/UltraTouchSX.ino
@@ -7,21 +7,61 @@
  *   \___/ |_| \__||_|    \__,_|  \_/   \___/  \__,_| \___||_| |_| \____/ \/   \/                                                                          
  *   
  * 
- * Die Version der UltraTouch SX (Version 6) ist ausgestattet mit einem Nextion Display 
- * und sie versendet MIDI zur Nutzung der Räuspertasten. Sie kann vielseitig genutzt 
- * und erweitert werden ;)
+ * Die Version der UltraTouch SX (Version 6) ist ausgestattet mit einem Nextion Display und sie versendet MIDI zur Nutzung 
+ * der Räuspertasten. Sie kann vielseitig genutzt und erweitert werden ;)
  * 
- * Diese Projekt entstand im Rahmen des Ultraschall Projektes zum Themea: "MIDI Controller"
- * Es gibt viele weitere Projekte von meinen Kollegen und der Community.
- * Schaue doch mal vorbei: https://ultraschall.fm
+ * Diese Projekt entstand im Rahmen des Ultraschall Projektes zum Themea: "MIDI Controller". Es gibt viele weitere Projekte 
+ * von meinen Kollegen und der Community. Schaue doch mal vorbei: https://ultraschall.fm
  * 
  * @developer   Michael McCouman Jr.
  * @version     UT[SX] V6
  * @date        4.Okt 2018
+ * @git:        https://github.com/McCouman
  * @website     https://wikibyte.org
  * 
- * Licence:     CC BY SA    
- * Github:      https://github.com/McCouman
+ * Licence:     CC BY-NC-SA 4.0
+ * Link:        https://creativecommons.org/licenses/by-nc-sa/4.0/deed.de
+ *
+**/
+
+/*
+ 
+   INFORMATIONEN ZUR LIZENZ:
+   
+   Diese Software steht unter CC BY-NC-SA 4.0 Lizenz: Namensnennung - Nicht-kommerziell - Weitergabe unter gleichen Bedingungen 
+   4.0 International (CC BY-NC-SA 4.0). Dies ist eine allgemeinverständliche Zusammenfassung der Lizenz (die diese nicht ersetzt). 
+   
+   HAFTUNGSBESCHRÄNKUNG:
+   
+   Sie dürfen:
+   - Teilen — das Material in jedwedem Format oder Medium vervielfältigen und weiterverbreiten
+   - Bearbeiten — das Material remixen, verändern und darauf aufbauen
+   - Der Lizenzgeber kann diese Freiheiten nicht widerrufen solange Sie sich an die Lizenzbedingungen halten.
+   
+   Unter folgenden Bedingungen:
+   - Namensnennung — Sie müssen angemessene Urheber- und Rechteangaben machen, einen Link zur Lizenz beifügen und angeben, 
+     ob Änderungen vorgenommen wurden. Diese Angaben dürfen in jeder angemessenen Art und Weise gemacht werden, allerdings 
+     nicht so, dass der Eindruck entsteht, der Lizenzgeber unterstütze gerade Sie oder Ihre Nutzung besonders.
+   
+   - Nicht kommerziell — Sie dürfen das Material nicht für kommerzielle Zwecke nutzen.
+   
+   - Weitergabe unter gleichen Bedingungen — Wenn Sie das Material remixen, verändern oder anderweitig direkt darauf aufbauen, 
+     dürfen Sie Ihre Beiträge nur unter derselben Lizenz wie das Original verbreiten.
+   
+   - Keine weiteren Einschränkungen — Sie dürfen keine zusätzlichen Klauseln oder technische Verfahren einsetzen, 
+     die anderen rechtlich irgendetwas untersagen, was die Lizenz erlaubt.
+   
+   
+   Hinweise:
+   Sie müssen sich nicht an diese Lizenz halten hinsichtlich solcher Teile des Materials, die gemeinfrei sind, 
+   oder soweit Ihre Nutzungshandlungen durch Ausnahmen und Schranken des Urheberrechts gedeckt sind. Es werden keine Garantien 
+   gegeben und auch keine Gewähr geleistet. Die Lizenz verschafft Ihnen möglicherweise nicht alle Erlaubnisse, die Sie für die 
+   jeweilige Nutzung brauchen. Es können beispielsweise andere Rechte wie Persönlichkeits- und Datenschutzrechte zu beachten sein, 
+   die Ihre Nutzung des Materials entsprechend beschränken.
+   
+   Link:
+   - https://creativecommons.org/licenses/by-nc-sa/4.0/deed.de
+
  * 
 */
 
